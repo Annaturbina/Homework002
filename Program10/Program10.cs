fc46c92c354bcf35ab38f278275c445713ab4ae0
Anna <turbina.anna97@yandex.ru>
2022-11-27 21:52:54 +0300

    Final code

--- Program10/Program10.cs
+++ Program10/Program10.cs
@@ -1,1 +1,8 @@
-﻿
+﻿ Console.WriteLine("Введите трёхзначное число");
+ var numbers = Convert.ToInt64(Console.ReadLine());
+        while (numbers >= 100)
+        {
+            numbers /= 10;
+        }
+        var digit = numbers % 10;
+        Console.WriteLine("Вторая цифра в числе = "+digit);
